93bab7ba6592f96f1ac45c05052aea74de3ca01d
Chupko11 <128851746+Chupko11@users.noreply.github.com>
2023-07-12 11:32:22 +0200

    -Fixed routes

--- routes/author.php
+++ routes/author.php
@@ -14,32 +14,33 @@ Route::prefix('author')->name('author.')->group(function(){
     //Ova linija koda omogućuje osobama koji nisu korisnici da otvore dva view-a (login i forgot-password)
         Route::middleware(['guest:web'])->group(function () {
             Route::get('/homeguest', [RegisterController::class,'index'])->name('homeGuest');
-            Route::get('/signup', [RegisterController::class, 'create'])->name('signup');
-            Route::post('/signup', [RegisterController::class, 'store'])->name('signupStore');
-            Route::post('/login', [RegisterController::class, 'login'])->name('Loginrequest');
-            Route::post('/forgot-password',[RegisterController::class, 'forgotPassword'])->name('forgot-password');//šalje se mail korisniku
-            Route::post('/reset-password', [RegisterController::class, 'resetPasswordSave'])->name('resetPasswordSave'); //sprema se novi password
-            Route::get('/reset-password/{token}', [RegisterController::class, 'resetPassword'])->name('resetPassword'); //otvara novi view gdje korisnik unosi novi password
+            Route::get('/homeguest/signup', [RegisterController::class, 'create'])->name('signup');
+            Route::post('/homeguest/signup', [RegisterController::class, 'store'])->name('signupStore');
+            Route::view('/homeguest/login','back.pages.auth.login')->name('login');
+            Route::post('/homeguest/login', [RegisterController::class, 'login'])->name('Loginrequest');
+            Route::post('/homeguest/forgot-password',[RegisterController::class, 'forgotPassword'])->name('forgot-password');//šalje se mail korisniku
+            Route::post('/homeguest/reset-password', [RegisterController::class, 'resetPasswordSave'])->name('resetPasswordSave'); //sprema se novi password
+            Route::get('/homeguest/reset-password/{token}', [RegisterController::class, 'resetPassword'])->name('resetPassword'); //otvara novi view gdje korisnik unosi novi password
         });
 
         Route::middleware(['auth:web'])->group(function() {
 
             Route::get('/home', [AuthorController::class,'index'])->name('home');
-            Route::post('/logout', [AuthorController::class, 'logout'])->name('logout');
-            Route::get('/profile', [AuthorController::class, 'profile'])->name('profile');
-            Route::post('/update-profile', [AuthorController::class, 'update'])->name('update');
-            Route::post('/profile/picture', [AuthorController::class, 'updateProfilePicture'])->name('pictureUpdate');
-            Route::post('/profile/password', [AuthorController::class, 'updatePasswordSave'])->name('postPasswordUpdate');
-            Route::post('/profile/delete-account', [AuthorController::class, 'deleteAccount'])->name('deleteAccount');
+            Route::post('/home/logout', [AuthorController::class, 'logout'])->name('logout');
+            Route::get('/home/profile', [AuthorController::class, 'profile'])->name('profile');
+            Route::post('/home/update-profile', [AuthorController::class, 'update'])->name('update');
+            Route::post('/home/profile/picture', [AuthorController::class, 'updateProfilePicture'])->name('pictureUpdate');
+            Route::post('/home/profile/password', [AuthorController::class, 'updatePasswordSave'])->name('postPasswordUpdate');
+            Route::post('/home/profile/delete-account', [AuthorController::class, 'deleteAccount'])->name('deleteAccount');
 
 
 
             Route::get('/posts/create', [PostController::class, 'create'] )->name('createPost');
             Route::post('/posts', [PostController::class, 'store'])->name('storePost');
-            Route::get('/showPosts', [PostController::class, 'show'])->name('showPosts');
-            Route::delete('/deletePost/{id}', [PostController::class, 'delete'])->name('deletePost');
-            Route::post('/updatePost/{id}', [PostController::class, 'updatePost'])->name('updatePost');
-            Route::post('/updatePost', [PostController::class, 'postUpdatePost'])->name('postUpdatePost');
+            Route::get('/posts/showPosts', [PostController::class, 'show'])->name('showPosts');
+            Route::post('/posts/updatePost', [PostController::class, 'postUpdatePost'])->name('postUpdatePost');
+            Route::delete('/posts/deletePost/{id}', [PostController::class, 'delete'])->name('deletePost');
+            Route::post('/posts/updatePost/{id}', [PostController::class, 'updatePost'])->name('updatePost');
             Route::post('/posts/{id}/like', [PostController::class, 'likePost'])->name('post.like');
             Route::post('/posts/{id}/unlike', [PostController::class, 'unlikePost'])->name('post.unlike');
 
@@ -71,7 +72,7 @@ Route::prefix('author')->name('author.')->group(function(){
 
 
 
-        Route::view('/login','back.pages.auth.login')->name('login');
+
         Route::view('/forgot-password','back.pages.auth.forgot')->name('forgot-passwordView');
         Route::get('/search', [PostController::class, 'search'])->name('searchPost');
         Route::get('/searchpost', [PostController::class, 'postSearchPost'])->name('postSearchPost');
